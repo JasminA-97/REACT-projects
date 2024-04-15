2d73a68c7bbfe56fe19acc1769b7a63367561d67
JasminA-97 <21mp1828@rit.ac.in>
2024-04-15 12:00:55 +0530

    React demo project

--- demo-project/src/App.jsx
+++ demo-project/src/App.jsx
@@ -3,10 +3,18 @@ import './App.css'
 import Compny from './Components/Compny'
 import Employee from './Components/Compny'
 import Car from './Components/Car'
+import { useState } from 'react'
 
 //component is simply a function
 function App() {
   //javascript code
+
+  // `````````````` PREDEFINED HOOK``````````````
+  // 1)useState(initial val) 
+  
+      const[username,setUsername]=useState("")
+ 
+  // `````````````` `````````````` ``````````````
   const empName='Jasmin'
   const h1Style={color:'white',backgroundColor:'black',textAlign:'center',padding:'10px'}
 
@@ -19,6 +27,16 @@ function App() {
       <Employee name={empName} status desig={false} check="emp values"/>
       <Compny/>
       <Car/>
+
+{/*`````````````` useState()``````````````*/}
+    <div className="text-center m-5">
+      <h4><u>usage of Hooks----useState(initial val)</u></h4>
+        <input onChange={e=>setUsername(e.target.value)} type="text" className="form-control m-5" placeholder='Enter employee name'/>
+    </div>
+    <h1 style={{height:'150px'}} className="m-5 text-primary bg-warning border text-center p-5">Employee name:{username}</h1>
+
+{/*`````````````` `````````````` `````````*/}
+
     </>
   )
 }
